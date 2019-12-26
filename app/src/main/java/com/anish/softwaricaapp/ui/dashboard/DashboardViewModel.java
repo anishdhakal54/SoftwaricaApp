92fcd8b64686dadfc315e24b6188a165532ad6ad
anishdhakal54 <anish.dhakal54@gmail.com>
2019-12-26 20:57:23 +0545

    adding student and showing in recycle view. sucessfully added

--- app/src/main/java/com/anish/softwaricaapp/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/anish/softwaricaapp/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("Add Student Record");
     }
 
     public LiveData<String> getText() {
